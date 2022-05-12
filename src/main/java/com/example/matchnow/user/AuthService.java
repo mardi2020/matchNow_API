7183c68b1b1fd571237310d06bf5faeabe255781
amdx1254 <amdx1254@gmail.com>
2022-05-13 01:07:37 +0900

    🌱 [add] Register, Login

---
+++ src/main/java/com/example/matchnow/user/AuthService.java
@@ -0,0 +1,46 @@
+package com.example.matchnow.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void join(UserJoinDTO userJoinDTO) {
+        validateDuplicateUser(userJoinDTO);
+        if(!checkValidEmail(userJoinDTO.getEmail()))
+            throw new IllegalStateException("올바르지 않는 email입니다.");
+
+        User user = new User();
+        user.setEmail(userJoinDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(userJoinDTO.getPassword()));
+        user.setUsername(userJoinDTO.getUsername());
+
+        userRepository.join(user);
+    }
+
+    private void validateDuplicateUser(UserJoinDTO userJoinDTO) {
+        List<User> users = userRepository.findByEmail(userJoinDTO.getEmail());
+        if(!users.isEmpty())
+            throw new IllegalStateException("이미 가입되어 있는 email입니다.");
+    }
+
+    private boolean checkValidEmail(String email) {
+        String regexPattern = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        return Pattern.compile(regexPattern)
+                .matcher(email)
+                .matches();
+    }
+}
---
+++ src/main/java/com/example/matchnow/user/UserJoinDTO.java
@@ -0,0 +1,14 @@
+package com.example.matchnow.user;
+
+import lombok.Data;
+
+@Data
+public class UserJoinDTO {
+
+    private String email;
+
+    private String password;
+
+    private String username;
+
+}
